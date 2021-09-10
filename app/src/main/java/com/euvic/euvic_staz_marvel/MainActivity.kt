0bf24d2c10a757af62dfe63c7053a2af7f01326f
yngdrc <kacper.heller@edu.uekat.pl>
2021-09-10 18:15:18 +0200

    Initial commit

---
+++ app/src/main/java/com/euvic/euvic_staz_marvel/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.euvic.euvic_staz_marvel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
